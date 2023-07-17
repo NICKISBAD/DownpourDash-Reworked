e222dbe8b9d1ea8d823e00ecd61a845515037251
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-07-18 00:01:35 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,31 +1,48 @@
 local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
 
 local function pb(b)
-           game:GetService("Workspace").Current.CurrentMap.Buttons [b].Head.CFrame = plr.CFrame
+           plr.CFrame = game:GetService("Workspace").Current.CurrentMap.Buttons [b].Head.CFrame 
+end
+
+local function win()
+    game.Workspace.Current.CurrentMap.ExitRegion.CFrame = plr.CFrame
 end
 
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/InfinitiveUI",true))()
 
 
 --Lib:CreateWindow(name,DefTab,WinSize,function)
-local Win = Lib:CreateWindow("Nick's DP Dash",30,nil,nil)
+local Win = Lib:CreateWindow("Nick's DP Dash",90,nil,nil)
 local Tab,name = Win:CreateTab("DP Dash Gui",function() end)
 
-Tab:CreateButton("Press all buttons (move in circles" ,function()
+
+
+Tab:CreateButton("Auto win/Press all buttons" ,function()
 pb("Button1")
+wait(0.3)
 pb("Button2")
+wait(0.3)
 pb("Button3")
+wait(0.3)
 pb("Button4")
+wait(0.3)
 pb("Button5")
+wait(0.3)
 pb("Button6")
+wait(0.3)
 pb("Button7")
+wait(0.3)
 pb("Button8")
+wait(0.3)
 pb("Button9")
+wait(0.3)
 pb("Button10")
+wait(0.3)
 pb("Button11")
+wait(0.3)
 pb("Button12")
+wait(0.3)
 pb("Button13")
-    
 end)
 
 Tab:CreateButton("Exit (all buttons must be pressed" ,function()
@@ -36,4 +53,14 @@ end)
 Tab:CreateButton("Delete water (w.i.p)" ,function()
         game:GetService("Workspace").Current.CurrentMap.Liquid:Destroy()
 game:GetService("Workspace").Current.CurrentMap:WaitForChild("Water1"):Destroy()
+game:GetService("Workspace").Current.CurrentMap:WaitForChild("Water2"):Destroy()
+game:GetService("Workspace").Current.CurrentMap:WaitForChild("Water3"):Destroy()
+game:GetService("Workspace").Current.CurrentMap:WaitForChild("Water4"):Destroy()
+game:GetService("Workspace").Current.CurrentMap:WaitForChild("Water5"):Destroy()
 end)
+
+game:GetService("StarterGui"):SetCore("SendNotification",{ 	
+    Title = "Note", 	
+    Text = "If the features dont work on mobile, re-execute the script!",
+    Duration = 5
+})
